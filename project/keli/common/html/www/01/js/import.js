4bf51d98059f3295e840c15a39736f5b50a5b500
aortm2 <94888466+aortm2@users.noreply.github.com>
2023-09-11 14:50:25 +0900

    Delete project directory

--- project/keli/common/html/www/01/js/import.js
+++
@@ -1,7 +0,0 @@
-document.write('<script src="js/globalvars.js"></script>');
-document.write('<script src="js/jquery-latest.js"></script>');
-document.write('<script src="js/jquery-ui.min.js"></script>');
-document.write('<script src="js/video-5.min.js"></script>');
-document.write('<script src="js/quiz.js"></script>');
-document.write('<script src="js/menu.js"></script>');
-document.write('<script src="js/jquery.bxslider.js"></script>');
