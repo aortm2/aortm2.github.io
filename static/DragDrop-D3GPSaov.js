0f8758bacc8d491df6b0523f4f31124ddafe4d85
문정현 <aortm2@gmail.com>
2024-09-27 10:46:32 +0900

    으흠?

--- static/DragDrop-D3GPSaov.js
+++
@@ -1,2 +0,0 @@
-import{c as e,o as a,a as r}from"./index-Su1-zZVB.js";const t={class:"iframe-wrap"},o=r("iframe",{src:"/daechwita/index.html",frameborder:"0"},null,-1),s=[o],d={__name:"DragDrop",setup(c){return(_,n)=>(a(),e("div",t,s))}};export{d as default};
-//# sourceMappingURL=DragDrop-D3GPSaov.js.map
---
+++ static/js/DragDrop-D4t-H7Md.js
@@ -0,0 +1,1 @@
+import{c as e,o as a,a as r}from"./index-DqwhbnEc.js";const t={class:"iframe-wrap"},o=r("iframe",{src:"/daechwita/index.html",frameborder:"0"},null,-1),s=[o],d={__name:"DragDrop",setup(c){return(_,n)=>(a(),e("div",t,s))}};export{d as default};
